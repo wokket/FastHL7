a44a52a1ca9c2f624f51599b6296bf29c12af4b5
Tim Thompson <wokket@gmail.com>
2025-09-25 18:16:55 +1000

    chore: Removed reference to bug and made code the same now HL7V2 has fixed their microsecond bug

--- src/Benchmarks/DateTimeParseBench.cs
+++ src/Benchmarks/DateTimeParseBench.cs
@@ -14,14 +14,14 @@ public class DateTimeParseBench
 { 
 
 /*
-| Method                  | Mean      | Ratio | Gen0   | Allocated | Alloc Ratio |
-|------------------------ |----------:|------:|-------:|----------:|------------:|
-| FastHL7_ParseDateValues |  61.87 ns |  1.00 |      - |         - |          NA |
-| HL7V2_ParseDateValues   | 976.06 ns | 15.81 | 0.1793 |    2824 B |          NA |
-| NHapi_ParseDateValues   | 188.09 ns |  3.05 | 0.0489 |     768 B |          NA |
+| Method                  | Mean      | Ratio | Gen0   | Gen1   | Allocated | Alloc Ratio |
+|------------------------ |----------:|------:|-------:|-------:|----------:|------------:|
+| FastHL7_ParseDateValues |  51.63 ns |  1.00 |      - |      - |         - |          NA |                                                                                                                                            
+| HL7V2_ParseDateValues   | 866.87 ns | 16.79 | 0.1631 | 0.0010 |    2824 B |          NA |
+| NHapi_ParseDateValues   | 169.98 ns |  3.29 | 0.0443 |      - |     768 B |          NA |
 */
 
-    // Again, all tests are flawed, but lets try as best we can
+    // Again, all tests are flawed, but let's try as best we can
 
     private const string _inputDT = "202503"; // March 2025
     private const string _inputTS = "20250304132813.1234+0930"; // March 4th, 2025, 1:28:13 PM, with offset +9:30
@@ -36,7 +36,9 @@ public class DateTimeParseBench
         }
 
         var ts = _inputTS.AsSpan().AsDateTime();
-        if (ts != new DateTimeOffset(2025, 03, 04, 13, 28, 13, new(9, 30, 0)).AddSeconds(0.1234)) //sanity check
+        var expected = new DateTimeOffset(2025, 03, 04, 13, 28, 13, new(9, 30, 0))
+            .AddSeconds(0.1234);
+        if (ts != expected) //sanity check
         {
             throw new("Invalid TS value");
         }
@@ -53,9 +55,8 @@ public class DateTimeParseBench
 
         var ts = MessageHelper.ParseDateTime(_inputTS);
 
-        // HL7V2 seems to lose the microsecond???  Logged at https://github.com/Efferent-Health/HL7-V2/issues/12
         var expected = new DateTime(2025, 03, 04, 13, 28, 13, DateTimeKind.Utc).Subtract(new TimeSpan(9, 30, 0))
-            .AddSeconds(0.123);
+            .AddSeconds(0.1234);
         if (ts!.Value != expected) //sanity check
         {
             throw new("Invalid TS value");
@@ -75,7 +76,8 @@ public class DateTimeParseBench
         
         var tsField = new CommonTS(_inputTS);
         var dtm = new DateTimeOffset(tsField.Year, tsField.Month, tsField.Day, tsField.Hour, tsField.Minute, tsField.Second,  TimeSpan.FromMinutes(570)); // +9:30 offset, I'm not smart enough to get it from ts
-        if (dtm != new DateTimeOffset(2025, 03, 04, 13, 28 , 13, TimeSpan.FromHours(9).Add(TimeSpan.FromMinutes(30)))) //sanity check
+        var expected = new DateTimeOffset(2025, 03, 04, 13, 28, 13, TimeSpan.FromHours(9).Add(TimeSpan.FromMinutes(30)));
+        if (dtm != expected) //sanity check
         {
             throw new("Invalid date value");
         }
